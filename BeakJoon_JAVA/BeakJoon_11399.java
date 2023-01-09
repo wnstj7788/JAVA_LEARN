c32bf445a83fa07b51ec3f6e5c111a75cf91eb89
gom <61938768+wnstj7788@users.noreply.github.com>
2023-01-09 09:50:20 +0900

    BJ_11399

--- BeakJoon_JAVA/BeakJoon_11399.java
+++ BeakJoon_JAVA/BeakJoon_11399.java
@@ -1,8 +1,35 @@
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class BeakJoon_11399 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		
+		int[] arr = new int[num];
+		
+		for(int i = 0; i <num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		int total_time = 0;
+		int sum = 0;
+		for(int i =0; i < num; i++) {
+			total_time += arr[i] + sum;
+			sum += arr[i];
+			
+			
+		}
+		//1 2 3 3 4
+		System.out.println(total_time);
+		
+		
+
+		
 
 	}
 
