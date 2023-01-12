dcc63c31aa70f7be07e98d9d2818ca2593ba1f61
wnstj7788 <61938768+wnstj7788@users.noreply.github.com>
2023-01-12 21:17:19 +0900

    BaekJoon_16435

--- BaekJoon_Java/BaekJoon_1026.java
+++ BaekJoon_Java/BaekJoon_1026.java
@@ -19,12 +19,12 @@ public class BaekJoon_1026 {
 			arr_b[i] = sc.nextInt();
 		}
 		
-		Arrays.sort(arr_a);
+		Arrays.sort(arr_a);//오름차순 배열 
 		Integer[] arr_b2 = Arrays.stream(arr_b).boxed().toArray(Integer[]::new);
-		Arrays.sort(arr_b);
+		Arrays.sort(arr_b); //내림차순 배
 		int sum = 0;
 		for(int i = 0; i <n; i++) {
-			sum += arr_a[i] * arr_b[n-1 - i];
+			sum += arr_a[i] * arr_b[n-1 - i]; // 에이는 첫번쨰 요소부터 b는 마지막 요소주터 꺼내 곱하기 
 		}
 		System.out.println(sum);
 	}
---
+++ BaekJoon_Java/BaekJoon_16435.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_16435 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int L = sc.nextInt();
+
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+
+		for (int i = 0; i < N; i++) {
+			if (arr[i] <= L ) {
+				L++;
+				
+			} else {
+				break;
+			}
+		}
+		System.out.println(L);
+
+	}
+}
+
